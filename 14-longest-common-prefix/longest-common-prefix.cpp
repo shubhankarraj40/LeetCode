e60c0e061b74917d419b592339dbd1f0c58dbc51
Shubhankar Raj <mritunjaymishra8953@gmail.com>
2025-06-09 01:45:01 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (33.68%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -5,7 +5,7 @@ public:
 
         string prefix = strs[0];
 
-        for(int i = 0; i < strs.size(); i++){
+        for(int i = 1; i < strs.size(); i++){
             while(strs[i].find(prefix) != 0){
             prefix = prefix.substr(0, prefix.length()-1);
             if(prefix.empty() ) return "";
